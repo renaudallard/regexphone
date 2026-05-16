7d868ae12ee0cd0c4eb7458c6b3b813b87f02cd9
Renaud Allard <renaud@allard.it>
2026-05-16 14:31:29 +0200

    Bump version to 0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "it.allard.regexphone"
         minSdk = 31
         targetSdk = 35
-        versionCode = 11
-        versionName = "0.1.0"
+        versionCode = 12
+        versionName = "0.1.1"
     }
 
     signingConfigs {
